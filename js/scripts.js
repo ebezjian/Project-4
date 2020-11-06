e458cc92bcb1b219801caffa014ae2ca07f0ec9e
Ellie Lambert <ebezjian@gmail.com>
2020-11-06 15:36:12 -0500

    add append to checkout list to show what user has ordered

--- js/scripts.js
+++ js/scripts.js
@@ -7,6 +7,7 @@ function PizzaOrder(sizes, toppings, cost) {
 };  
 //let newOrder = new PizzaOrder ("Large",["Mushroom"], 4);
 
+//this is a prototype which only has a simple formula for taking in sizes as the main price point. Depending on which size the user selects, regardless of topppings, it will add up these prices.
 PizzaOrder.prototype.priceAmount = function(){
   let price = 0;
   if (this.sizes === "Large") {
@@ -21,15 +22,19 @@ PizzaOrder.prototype.priceAmount = function(){
   }
   return price * this.cost;
 }
+
+
 //User Logic
 $(document).ready(function(){
   $("form.orderForm").submit(function(event){
     event.preventDefault();
     const inputSize = $("#sizes").find(":selected").text();
     const inputToppings = $("#toppings").find(":selected").text();
-    const amount = $("#amount").val();
+    let amount = $("#amount").val();
     let newPizza = new PizzaOrder(inputSize, inputToppings, amount);
+    console.log(newPizza);
     $("#totalCost").text("$" + newPizza.priceAmount());
+    $("#checkList").append("<li>" + newPizza.sizes + newPizza.toppings + newPizza.cost + "</li>");
   })
 })
 
